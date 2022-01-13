59d97e331f83bf30086a76bd556904db8c914a40
XiaXingYi <2411138817@qq.com>
2022-01-13 11:18:32 +0800

    完成传感器数据解析

---
+++ src/main/java/com/wsn/nac/MqttApplication.java
@@ -0,0 +1,20 @@
+package com.wsn.nac;
+
+import com.spring4all.swagger.EnableSwagger2Doc;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+@EnableScheduling
+@EnableSwagger2Doc
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MqttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqttApplication.class, args);
+    }
+
+}
